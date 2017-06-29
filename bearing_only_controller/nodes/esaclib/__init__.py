e7383d0ffd326a6719b27533874bd463c05414bb
Ian Abraham <ianabraham21@gmail.com>
2017-06-29 14:57:09 -0500

    updated branch

--- bearing_only_controller/nodes/esaclib/__init__.py
+++
@@ -1,7 +0,0 @@
-from single_integrator import Single_Integrator
-from double_integrator import DoubleIntegrator
-from basis import Basis
-from basis0 import Basis as Basis0
-from erg_cost_func import CostFunction
-from SAC import SAC
-from sensor import BearingOnly
--- tactile_esac/nodes/sac_lib/__init__.py
+++
@@ -1,5 +0,0 @@
-from single_integrator import Single_Integrator
-from sensor import Tactile_Sensor
-from erg_cost_func import cost_functional
-from basis import basis
-from SAC import SAC
